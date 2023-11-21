c1a8caaa31dcea612a352d5ec1742c4c3df48150
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-11-21 15:00:22 -0500

    Datos de usuario

---
+++ ejercicio.c
@@ -0,0 +1,18 @@
+/*Cree una matriz cuya dimensiones sean ingresadas por el usuario.
+La matriz puede tener cualquier dimension
+Inicialice toda la matriz en ceros
+Imprima la matriz con la diagonal principal con valores de 1*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    printf("Ingrese el numero de filas -> ");
+    scanf("%d", &n);
+    printf("Ingrese el numero de columnas -> ");
+    scanf("%d", &m);
+
+    
+    return 0;
+}
